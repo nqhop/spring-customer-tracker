472a83fb9a45e3c0a8cbb7d73e7aa2e28ae89c16
nqhop <hop35889@gmail.com>
2023-04-11 13:42:14 +0700

    add jsp

---
+++ src/com/NguyenQuocHop/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,10 @@
+package com.NguyenQuocHop.springdemo.dao;
+
+import java.util.List;
+
+import com.NguyenQuocHop.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+}
