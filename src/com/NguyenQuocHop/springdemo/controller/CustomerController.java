82603fc9fd625c8963fe0f3a8f439fcb4789f5fb
nqhop <hop35889@gmail.com>
2023-04-19 21:06:53 +0700

    add delete method

--- src/com/NguyenQuocHop/springdemo/controller/CustomerController.java
+++ src/com/NguyenQuocHop/springdemo/controller/CustomerController.java
@@ -72,4 +72,12 @@ public class CustomerController {
 		return "customer-form";
 	}
 	
+	@GetMapping("delete")
+	public String deleteCustomer(@RequestParam("customerId") int theId,
+			Model theModel) {
+		customerService.deleteCustomer(theId);
+		
+		return "redirect:/customer/list";
+	}
+	
 } 
